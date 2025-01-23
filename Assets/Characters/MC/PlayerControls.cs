c8b423c0e1356401ccd9322bb66073a02c4058e0
galefields <99187681+galefields@users.noreply.github.com>
2025-01-23 22:58:55 +0800

    joystick

---
+++ Assets/Characters/MC/PlayerControls.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControls : MonoBehaviour
+{
+    [SerializeField] private InputActionReference moveActionToUse;
+    [SerializeField] private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveDirection = moveActionToUse.action.ReadValue<Vector2>();
+        transform.Translate(moveDirection * speed * Time.deltaTime);
+    }
+}
