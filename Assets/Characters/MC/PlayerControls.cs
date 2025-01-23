7e31281fdba94d78dfb75bd0beffcc96e2460aeb
galefields <99187681+galefields@users.noreply.github.com>
2025-01-24 02:04:33 +0800

    movement flips and joystick
    weapon flips and switches to other side based on movement

--- Assets/Characters/MC/PlayerControls.cs
+++ Assets/Characters/MC/PlayerControls.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
     [SerializeField] private InputActionReference moveActionToUse;
     [SerializeField] private float speed;
+    public float XY;
+    public float YX;
+    public Vector2 currentDirection;
+    [SerializeField] private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        Vector2 moveDirection = moveActionToUse.action.ReadValue<Vector2>();
+        transform.Translate(moveDirection * speed * Time.deltaTime);
+
+        currentDirection = moveDirection;
     }
 
     // Update is called once per frame
@@ -18,5 +30,18 @@ public class PlayerControls : MonoBehaviour
     {
         Vector2 moveDirection = moveActionToUse.action.ReadValue<Vector2>();
         transform.Translate(moveDirection * speed * Time.deltaTime);
+        currentDirection = moveDirection;
+        XY = moveDirection.x + moveDirection.y * speed;
+        YX = moveDirection.x + moveDirection.y;
+        animator.SetFloat("Speed", Mathf.Abs(XY));
+
+        if (moveDirection.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (moveDirection.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
     }
 }
--- Assets/Scripts/locking.cs
+++ Assets/Scripts/locking.cs
@@ -1,17 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class locking : MonoBehaviour
 {
+    public PlayerControls playerControls;
     public GameObject parentObject; // Assign the parent GameObject in the Inspector
     public Vector3 offset; // Set the desired relative position in the Inspector
+    public Vector2 direction;
+    public Vector3 flip;
+    
 
+    private SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        playerControls = parentObject.GetComponent<PlayerControls>();
         transform.parent = parentObject.transform;
         transform.localPosition = offset;
+        flip = -offset;
+
+       
+
     }
 
     // Update is called once per frame
@@ -19,5 +31,15 @@ public class locking : MonoBehaviour
     {
         transform.parent = parentObject.transform;
         transform.localPosition = offset;
+        if (playerControls.currentDirection.x < 0)
+        {
+            spriteRenderer.flipX = false;
+            offset.x = flip.x;
+        }
+        else if (playerControls.currentDirection.x > 0)
+        {
+            spriteRenderer.flipX = true;
+            offset.x = -flip.x;
+        }
     }
 }
