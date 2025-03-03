7aae2c8e44b99da7f420fb78b7f031285d1a147d
Arbie Santino D Castillo <141717585+Hydragon222@users.noreply.github.com>
2025-03-03 21:19:50 +0800

    Update level manager: bridged level menu buttons to levels (level 11 and 12 buttons are not created yet)

--- Assets/Arbie/Scene/Main Menu/Scripts/LevelManager.cs
+++ Assets/Arbie/Scene/Main Menu/Scripts/LevelManager.cs
@@ -12,6 +12,7 @@ public class LevelManager : MonoBehaviour
         AudioManager.instance.Stop("Menu");
         AudioManager.instance.Play("Practice Level Theme");
     }
+
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         AudioManager.instance.PlaySceneMusic(scene.name);
@@ -22,25 +23,36 @@ public class LevelManager : MonoBehaviour
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
-    // Load Level 1
-    public void LoadLevel1()
+    // Load Level 1 to Level 12
+    public void LoadLevel1() { LoadLevel(1); }
+    public void LoadLevel2() { LoadLevel(2); }
+    public void LoadLevel3() { LoadLevel(3); }
+    public void LoadLevel4() { LoadLevel(4); }
+    public void LoadLevel5() { LoadLevel(5); }
+    public void LoadLevel6() { LoadLevel(6); }
+    public void LoadLevel7() { LoadLevel(7); }
+    public void LoadLevel8() { LoadLevel(8); }
+    public void LoadLevel9() { LoadLevel(9); }
+    public void LoadLevel10() { LoadLevel(10); }
+    public void LoadLevel11() { LoadLevel(11); }
+    public void LoadLevel12() { LoadLevel(12); }
+
+    // Load any level dynamically from level1 to level12
+    public void LoadLevel(int levelNumber)
     {
-        Debug.Log("Loading Level 1...");
-        SceneManager.LoadScene("Level 1");
+        if (levelNumber < 1 || levelNumber > 12)
+        {
+            Debug.LogError("Invalid level number! Level must be between 1 and 12.");
+            return;
+        }
+
+        string levelName = "level" + levelNumber;
+        Debug.Log("Loading " + levelName + "...");
+        SceneManager.LoadScene(levelName);
+
         AudioManager.instance.Play("Clicking Sound");
         AudioManager.instance.Stop("Menu");
-        AudioManager.instance.Play("Level 1 Theme");
-    }
-
-    // Load Level 2
-    public void LoadLevel2()
-    {
-        Debug.Log("Loading Level 2...");
-        SceneManager.LoadScene("Level 2");
-        AudioManager.instance.Play("Clicking Sound");
-        AudioManager.instance.Stop("Menu");
-        AudioManager.instance.Play("Level 2 Theme");
-
+        AudioManager.instance.Play("Level " + levelNumber + " Theme");
     }
 
     // Return to the Main Menu
@@ -59,5 +71,4 @@ public class LevelManager : MonoBehaviour
             AudioManager.instance.Play("Menu");
         }
     }
-    
 }
